8aabef1b73039e37e9d9d8f308eace41a9c2a4c3
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-04-18 23:58:57 +0200

    Correzioni Script per Movimento

--- Assets/MovimentoPersonaggio.cs
+++ Assets/MovimentoPersonaggio.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class MovimentoPersonaggio : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
 
     public Animator animator;
 
-    Vector2 movement; 
+    Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -31,10 +30,9 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    void FixedUpdate() 
+    void FixedUpdate()
     {
         // Gestiamo all'interno di questa funzione i MOVIMENTI
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-
 }
