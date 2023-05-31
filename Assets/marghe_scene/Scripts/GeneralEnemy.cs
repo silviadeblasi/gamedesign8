8fc82a4a8916d0c395a2d7e3cd5b19bee8db95ab
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-05-31 02:38:59 +0200

    Implementato meccanismo di inseguimento

--- Assets/marghe_scene/Scripts/GeneralEnemy.cs
+++ Assets/marghe_scene/Scripts/GeneralEnemy.cs
@@ -7,7 +7,8 @@ public class GeneralEnemy : MonoBehaviour
     public int health;
     public string enemyName;
     public int baseAttack;
-    
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
