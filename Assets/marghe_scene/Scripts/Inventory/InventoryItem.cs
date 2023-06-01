a97da84e519bc36408390e19254a48b40a8d1b43
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-06-01 03:11:13 +0200

    Inventory pt1 - UI base
    La grafica fa schifo

---
+++ Assets/marghe_scene/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class InventoryItem : ScriptableObject
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemImage;
+    public int numberHeld;
+    public bool usable;
+    public bool unique;
+}
