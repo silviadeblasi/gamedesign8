6dc7953382d5c756f1e180d80e23d629915ae367
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-06-02 18:07:18 +0200

    main menu inizio
    aggiunto script per salvataggio e caricamento 
    set up main menu 
    scalato start menu ok

---
+++ Assets/silvia_scene/script_sylvia/save_load/player.cs
@@ -0,0 +1,41 @@
+using System.Collections; 
+using System.Collections.Generic;
+using UnityEngine;
+
+//qui implementerei i metodi per cambiare il livello e la vita del player
+
+public class player : MonoBehaviour {
+    
+    public int level  = 3;
+    public int health = 100;
+
+    public void SavePlayer() {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer() {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        level  = data.level;
+        health = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+
+        transform.position = position;
+    }
+    
+    #region UI Methods
+
+    public void ChangeLevel(int amount) {
+        level+= amount;
+    }
+
+    public void ChangeHealth(int amount) {
+        health += amount;
+    }
+
+    #endregion
+}
