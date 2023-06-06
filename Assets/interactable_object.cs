66cc1721efcc9925a6eefc6e213bb2d31670fa21
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-06-06 11:34:09 +0200

    canvas tomba e script per tutti gli oggetti con cui posso interagire

--- Assets/interactable_object.cs
+++ Assets/interactable_object.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class interactable_object : MonoBehaviour
 {
     [SerializeField] private GameObject Canvas_tomba;
+    [SerializeField] private GameObject Canvas_perchiusura; //per ricordare all'utente che premendo Z chiude il canvas
 
     private GameObject dialogueBoxClone;
+    private GameObject dialogueBoxClone2; //per ricordare all'utente che premendo Z chiude il canvas
     public GameObject player; 
-
+    private Animator animator;
+    private bool canvas_already_spawned = false;
     //attenzione è ontrigger se voglio chiudere il cavas devo usare ontriggerexit/stay e non oncollisionexit/stay
     //blocca il player
-    private void OnTriggerEnter2D(Collider2D other) {
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
 
         if(other.gameObject.layer == 8){ //creo un layer per la visione dei canvas relativi a questo script (layer 8)
-            dialogueBoxClone = (GameObject)GameObject.Instantiate(Canvas_tomba, transform.position, Quaternion.identity);
+            if(Input.GetKeyDown(KeyCode.X)){
+                if (canvas_already_spawned == false)
+                {
+                   dialogueBoxClone = (GameObject)GameObject.Instantiate(Canvas_tomba, transform.position, Quaternion.identity);
+                   player.GetComponent<PlayerMovement>().enabled = false;
+                    animator.SetBool("moving", false);
+                    canvas_already_spawned = true;
+                }
+                if (canvas_already_spawned == true)
+                {
+                    dialogueBoxClone2 = (GameObject)GameObject.Instantiate(Canvas_perchiusura, transform.position, Quaternion.identity);
+                    Destroy(dialogueBoxClone2, 3f);
+                }
+            }   
+           /* if(Input.GetKeyDown(KeyCode.Z)){
+                Destroy(dialogueBoxClone, 0.5f);
+                player.GetComponent<PlayerMovement>().enabled = true;
+            }*/
         }
-        player.GetComponent<PlayerMovement>().enabled = false;
+       
     }
 
 
@@ -24,9 +48,10 @@ public class interactable_object : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.X)){
+        if(Input.GetKeyDown(KeyCode.Z)){
                 Destroy(dialogueBoxClone, 0.5f);
                 player.GetComponent<PlayerMovement>().enabled = true;
+                canvas_already_spawned = false;
             }
     }
 }
