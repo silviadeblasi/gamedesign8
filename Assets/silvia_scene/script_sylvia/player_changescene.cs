a684dc645f3c0514f73ec33b100e7609711d2bd9
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-06-06 23:30:22 +0200

    piccole modifiche pre salvataggio posizione

--- Assets/silvia_scene/script_sylvia/player_changescene.cs
+++ Assets/silvia_scene/script_sylvia/player_changescene.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//devo salvare la posizione precedente del player e controllare che se sta uscendo dalla casa non si attivi il trigger dell'ingresso
 
 public class player_changescene : MonoBehaviour
 {
+    public GameObject player; //inserisco il player in modo da salvare la sua posizione
+    private Vector3 player_pos; //salvo la posizione del player
     private void OnTriggerEnter2D(Collider2D other) {
 
         //casa sue
         if(other.gameObject.layer == 7){
             Scene_Loader.Load(Scene_Loader.Scene.Casa_sue);
+        }
+
+        //uscita casa
+        if(other.gameObject.layer == 10){
+            Scene_Loader.Load(Scene_Loader.Scene.Esterno_lev1);
         }
     }
 }
