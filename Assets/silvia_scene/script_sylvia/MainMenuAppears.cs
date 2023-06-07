4517d6f97642670bc4d30dcafa6730ff23d725cf
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-06-07 18:56:02 +0200

    rosalia

--- Assets/silvia_scene/script_sylvia/MainMenuAppears.cs
+++ Assets/silvia_scene/script_sylvia/MainMenuAppears.cs
@@ -6,18 +6,19 @@ using UnityEngine.UI;
 public class MainMenuAppears : MonoBehaviour
 {
     public GameObject MainMenu_prefab;
+    public GameObject player;
     void Update(){
      if (Input.GetKeyDown(KeyCode.Escape)) 
         {
             
             if (MainMenu_prefab.activeSelf == false)
             {
-                
                 MainMenu_prefab.SetActive(true);
+                player.GetComponent<PlayerMovement>().enabled = false;
             }
             else
             {
-                
+                player.GetComponent<PlayerMovement>().enabled = true;
                 MainMenu_prefab.SetActive(false);
             }
         }
