9900d6631c26ba7091474d0b043fa9bf16d36d57
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-06-09 23:38:50 +0200

    salvataggio posizione quasi pronti

--- Assets/silvia_scene/script_sylvia/scene_transition.cs
+++ Assets/silvia_scene/script_sylvia/scene_transition.cs
@@ -8,8 +8,19 @@ public class scene_transition : MonoBehaviour
     public Vector2 player_position;
     public vector_value player_storage;
     private void OnTriggerEnter2D(Collider2D other) {
+
+        //ingresso casa 
+        //solo che io voglio vedere i layer e non i tag
         if(other.CompareTag("Player") && !other.isTrigger){
+            Scene_Loader.Load(Scene_Loader.Scene.Casa_sue);
             player_storage.initialValue = player_position;
         }
+            
+        //uscita casa
+        if(other.CompareTag("Player") && !other.isTrigger){
+            Scene_Loader.Load(Scene_Loader.Scene.Esterno_lev1);
+            player_storage.initialValue = player_position;
+        }
+        
     }
 }
