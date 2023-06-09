def667b5858b989dbe65986fb781afaa283ef7fb
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-06-09 03:55:58 +0200

    Fine Inventory Manager e nuova UI
    - Risolti il problema dell'Inventory Manager
    - Aggiunta proposta per la UI dell'inventario (magari cambiamo l'arancio in rosso)

--- Assets/marghe_scene/Scripts/Inventory/InventoryManager.cs
+++ Assets/marghe_scene/Scripts/Inventory/InventoryManager.cs
@@ -32,7 +32,7 @@ public class InventoryManager : MonoBehaviour
             for(int i = 0; i < playerInventory.myInventory.Count; i++)
             {
                 GameObject temp = Instantiate(blankInventorySlot, 
-                    inventoryPanel.transform.position, Quaternion.identity);
+                    inventoryPanel.transform.position, inventoryPanel.transform.rotation, inventoryPanel.transform);
                 temp.transform.SetParent(inventoryPanel.transform);
                 InventorySlot newSlot = temp.GetComponent<InventorySlot>();
                 
