11ae659c1d62d739a0d37f3093706fc0e5fca050
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-06-12 01:37:35 +0200

    Inventory Saver

--- Assets/marghe_scene/Scripts/Inventory/PlayerInventory.cs
+++ Assets/marghe_scene/Scripts/Inventory/PlayerInventory.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/Player Inventory")]
+[System.Serializable]
 public class PlayerInventory : ScriptableObject
 {
     public List<InventoryItem> myInventory = new List<InventoryItem>();
