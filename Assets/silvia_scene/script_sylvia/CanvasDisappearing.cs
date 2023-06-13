332ae4c5d25df1071b73674a79375c8d5c2d0181
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-06-13 23:28:02 +0200

    danni

--- Assets/silvia_scene/script_sylvia/CanvasDisappearing.cs
+++ Assets/silvia_scene/script_sylvia/CanvasDisappearing.cs
@@ -5,13 +5,22 @@ using System.Collections;
 public class CanvasDisappearing : MonoBehaviour
 {
     public GameObject panelToDisappear;
+    public GameObject canvasToDisappear; // Riferimento al canvas da far scomparire
     public GameObject canvas;
     public float fadeDuration = 1f;
 
-    private IEnumerator Start()
+    private void Update() {
+        if(canvasToDisappear.activeSelf == true){
+            StartCoroutine(routine_damage());
+        }
+    }
+
+
+
+    private IEnumerator routine_damage()
     {
         yield return new WaitForSeconds(2f); // Opzionale: puoi aggiungere un ritardo prima che inizi a scomparire il canvas
-
+        fadeDuration = 1f;
         float elapsedTime = 0f;
         Image panelImage = panelToDisappear.GetComponent<Image>();
         Color originalColor = panelImage.color;
@@ -20,16 +29,19 @@ public class CanvasDisappearing : MonoBehaviour
 
         while (elapsedTime < fadeDuration)
         {
+            Debug.Log("Sono nel while");
             elapsedTime += Time.deltaTime;
             float t = elapsedTime / fadeDuration; // Calcola il valore normalizzato del tempo trascorso
-
+            Debug.Log("t: " + t);
             // Interpola tra il colore originale e il colore di destinazione in base al tempo
             Color lerpedColor = Color.Lerp(originalColor, targetColor, t);
             panelImage.color = lerpedColor;
-
+            Debug.Log("panelImage.color: " + panelImage.color);
             yield return null; // Attendere un frame prima di continuare
         }
-
-        canvas.SetActive(false); // Disattiva il pannello quando ha completato la scomparsa
+        
+        //canvas.SetActive(false); // Disattiva il pannello quando ha completato la scomparsa
+        canvasToDisappear.SetActive(false); // Disattiva il canvas quando ha completato la scomparsa
+        panelImage.color = originalColor; //reimposto alpha a 1 per i danni successivi 
     }
 }
