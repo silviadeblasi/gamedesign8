6a33a076576996564f95f86eede789b3c712a130
Lucente98 <s319443@studenti.polito.it>
2023-06-16 18:13:16 +0200

    death anim e e prove main menu

--- Assets/mainmenu_Manager.cs
+++ Assets/mainmenu_Manager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mainmenu_Manager : MonoBehaviour
+public class MainMenu_Manager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
