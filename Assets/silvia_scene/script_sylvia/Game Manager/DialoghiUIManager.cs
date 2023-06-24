cdedec612fddbb8cbebbecb3c26c1cf33237b149
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-06-24 19:15:03 +0200

    ave maria

--- Assets/silvia_scene/script_sylvia/Game Manager/DialoghiUIManager.cs
+++ Assets/silvia_scene/script_sylvia/Game Manager/DialoghiUIManager.cs
@@ -8,6 +8,7 @@ public class DialoghiUIManager : MonoBehaviour
     public GameObject[] UI; //ad esempio tutorial o altro che deve essere comunicato interente al gioco 
     private GameObject CloneDialogueBox; //clona il dialogo per poterlo usare in altre scene(non devo inserire il dialogo nella scena ma mi clona il riferimento nei prefab)
     public bool finedialogo = false; // variabile che uso nel game manager per capire se il dialogo è finito
+    private bool dialogo_iniziato = false;
     private void Awake() {
        
     }
@@ -19,12 +20,14 @@ public class DialoghiUIManager : MonoBehaviour
             Debug.Log("fine dialogo");
         }
     }
+
     public void StartDialoghi(string dialogo_name){
         GameObject dialogo = FindDialoghi(dialoghi, dialogo_name);
-
         if(dialogo != null){
+            dialogo_iniziato = true;
             CloneDialogueBox = (GameObject)GameObject.Instantiate(dialogo, transform.position, Quaternion.identity);
             dialogo_fine = (dialogo.transform.Find("dialogueBox")?.gameObject).GetComponent<dialogue_script>();
+            dialogo_fine = (CloneDialogueBox.transform.Find("dialogueBox")?.gameObject).GetComponent<dialogue_script>(); //trovo lo script che gestisce il dialogo
         }
         else{
             Debug.LogWarning("Dialogo non trovato: " + dialogo_name);
@@ -44,8 +47,16 @@ public class DialoghiUIManager : MonoBehaviour
         return null;
     }
 
-
-
-
-
+    //questa funzione la chiamero in update per controllare ogni frame il bool fine dialogo che sta nello script del dialogo
+    public bool FineDialogo(string dialogo_name){
+        GameObject dialogo = FindDialoghi(dialoghi, dialogo_name); //trovo il dialogo
+        //verifico che il dialogo è inziato 
+        if(dialogo_iniziato == true){
+            if(dialogo_fine.fine_dialogo == true){
+                Debug.Log(dialogo_fine.fine_dialogo);
+                return finedialogo = true;
+            }
+        }
+        return finedialogo = false;
+    }
 }
--- Assets/silvia_scene/script_sylvia/Game Manager/GameManager.cs
+++ Assets/silvia_scene/script_sylvia/Game Manager/GameManager.cs
@@ -10,23 +10,20 @@ public class GameManager : MonoBehaviour
     public SoundManager soundManager;
     public DialoghiUIManager dialoghiManager;
     public GameObject Camera;
+    private bool finedialogo = false;
     
     private void Update() {
 
         if( current_scene.ToString() == "Stanza_sue"){
 
+            finedialogo = dialoghiManager.FineDialogo("scena 1");
+            if(finedialogo == true){
+                    player.GetComponent<PlayerMovement>().enabled = true;
+            }
             if(firstTime){
                 player.GetComponent<PlayerMovement>().enabled = false;
                 FirstScene(Camera.GetComponent<Camera>());
 
-                if(dialoghiManager.finedialogo == true){
-                    player.GetComponent<PlayerMovement>().enabled = true;
-                }
-
-                /*if(dialoghiManager.finedialogo == true){
-                    player.GetComponent<PlayerMovement>().enabled = true;
-                }*/
-                //dialoghiManager.StartDialoghi("scena 1");
                 Debug.Log("Stanza_sue");
                 firstTime = false;
             }
