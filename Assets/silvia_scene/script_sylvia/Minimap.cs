1ca0794067df60afd0cce41111da36b86db4842d
Lucente98 <s319443@studenti.polito.it>
2023-06-27 14:28:33 +0200

    MiniMap in esterno

--- Assets/silvia_scene/script_sylvia/Minimap.cs
+++ Assets/silvia_scene/script_sylvia/Minimap.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -32,4 +32,32 @@ public class Minimap : MonoBehaviour
     {
         return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
     }
+}*/
+
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public RectTransform playerInMap;
+    public RectTransform mainMap;
+    public RectTransform minimap;
+
+    private void Update()
+    {
+        // Calcola la posizione normalizzata del giocatore sulla mappa principale
+        Vector3 normalizedPosition = mainMap.InverseTransformPoint(transform.position);
+
+        // Calcola la posizione mappata del giocatore sulla minimappa
+        Vector2 mappedPosition = new Vector2(
+            normalizedPosition.x / mainMap.rect.width * minimap.rect.width,
+            normalizedPosition.y / mainMap.rect.height * minimap.rect.height
+        );
+
+        // Aggiorna la posizione del giocatore sulla minimappa
+        playerInMap.localPosition = mappedPosition;
+    }
 }
+
+
+
+
