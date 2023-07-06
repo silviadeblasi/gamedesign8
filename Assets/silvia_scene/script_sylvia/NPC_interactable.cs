e6ccb4af98fa03f8c62bcbd138f9d74f211c3856
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-07-06 17:52:28 +0200

    dialoghi ubriacone

--- Assets/silvia_scene/script_sylvia/NPC_interactable.cs
+++ Assets/silvia_scene/script_sylvia/NPC_interactable.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class NPC_interactable : MonoBehaviour
 {
     private Animator anim;
-    private Animator anim2;
+    private Animator anim_veggente;
+    private Animator anim_ubriacone;
     public DialoghiUIManager dialoghiUIManager; //cerco i dialoghi e li faccio comparire
     public GameObject player;
     public Rigidbody2D rb;
@@ -18,12 +19,16 @@ public class NPC_interactable : MonoBehaviour
     private bool fine3 = false;
     private bool fine4 = false;
     private bool fine5 = false;
+    private bool fine6 = false;
+    private bool fine7 = false;
+    private bool fine8 = false;
 
 
     private void Start() {
 
         anim = GetComponent<Animator>();
-        anim2 = veggente.GetComponent<Animator>();
+        anim_veggente = veggente.GetComponent<Animator>();
+        anim_ubriacone = ubriacone.GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         rb_veggente = veggente.GetComponent<Rigidbody2D>();
         rb_ubriacone = ubriacone.GetComponent<Rigidbody2D>();
@@ -35,9 +40,9 @@ public class NPC_interactable : MonoBehaviour
         
             if(Input.GetKeyDown(KeyCode.X)){
                 Vector3 dir = veggente.GetComponent<BoundedNPC>().directionVector;
-                anim2.SetBool("interact", true);
-                anim2.SetFloat("moveX", dir.x);
-                anim2.SetFloat("moveY", dir.y);
+                anim_veggente.SetBool("interact", true);
+                anim_veggente.SetFloat("moveX", dir.x);
+                anim_veggente.SetFloat("moveY", dir.y);
                 rb_veggente.constraints = RigidbodyConstraints2D.FreezeAll;
                 veggente.GetComponent<BoundedNPC>().enabled = false;
                 rb.constraints = RigidbodyConstraints2D.FreezeAll;
@@ -68,13 +73,73 @@ public class NPC_interactable : MonoBehaviour
                 rb.constraints = RigidbodyConstraints2D.FreezeRotation;
                 rb_veggente.constraints = RigidbodyConstraints2D.None;
                 rb_veggente.constraints = RigidbodyConstraints2D.FreezeRotation;
-                anim2.SetBool("interact", false);
+                anim_veggente.SetBool("interact", false);
                 veggente.GetComponent<BoundedNPC>().enabled = true;
             }
 
         }
 
         if(other.gameObject.layer == 20){ //ubriacone
+        
+            if(Input.GetKeyDown(KeyCode.X)){
+                fine1 = false;
+                fine2 = false;
+                fine3 = false;
+                fine4 = false;
+                fine5 = false;
+                Vector3 dir = ubriacone.GetComponent<BoundedNPC>().directionVector;
+                anim_ubriacone.SetBool("interact", true);
+                anim_ubriacone.SetFloat("moveX", dir.x);
+                anim_ubriacone.SetFloat("moveY", dir.y);
+                rb_ubriacone.constraints = RigidbodyConstraints2D.FreezeAll;
+                ubriacone.GetComponent<BoundedNPC>().enabled = false;
+                rb.constraints = RigidbodyConstraints2D.FreezeAll;
+                player.GetComponent<PlayerMovement>().enabled = false;
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_1"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_1") == true && fine1 == false){
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_1"); 
+                fine1 = true;
+            }
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunk_1") == true && fine2 == false){
+                fine2 = true;
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_2"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_2") == true && fine3 == false){
+                fine3 = true;
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_2"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunke_2") == true && fine4 == false){
+                fine4 = true;
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_3"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_3") == true && fine5 == false){
+                fine5 = true;
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_4");
+            }
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunke_4") == true && fine6 == false){
+                fine6 = true;
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_5"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_5") == true && fine7 == false){
+                fine7 = true;
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_5"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunk_5") == true && fine8 == false){
+                fine8 = true;
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_3"); 
+                player.GetComponent<PlayerMovement>().enabled = true;
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rb_ubriacone.constraints = RigidbodyConstraints2D.None;
+                rb_ubriacone.constraints = RigidbodyConstraints2D.FreezeRotation;
+                anim_ubriacone.SetBool("interact", false);
+                ubriacone.GetComponent<BoundedNPC>().enabled = true;
+            }
+
+        }
+
+        /*if(other.gameObject.layer == 20){ //villager pistola
         
             if(Input.GetKeyDown(KeyCode.X)){
                 fine1 = false;
@@ -90,37 +155,50 @@ public class NPC_interactable : MonoBehaviour
                 ubriacone.GetComponent<BoundedNPC>().enabled = false;
                 rb.constraints = RigidbodyConstraints2D.FreezeAll;
                 player.GetComponent<PlayerMovement>().enabled = false;
-                dialoghiUIManager.StartDialoghi("dialogue_veggente_1"); //i bet you are looking for...
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_1"); 
             }
-            if(dialoghiUIManager.FineDialogo("dialogue_veggente_1") == true && fine1 == false){
-                dialoghiUIManager.StartDialoghi("dialogo_sueveggente_1"); //and how do you know
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_1") == true && fine1 == false){
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_1"); 
                 fine1 = true;
             }
-            if(dialoghiUIManager.FineDialogo("dialogo_sueveggente_1") == true && fine2 == false){
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunk_1") == true && fine2 == false){
                 fine2 = true;
-                dialoghiUIManager.StartDialoghi("dialogue_veggente_2"); //so qualcosa sul tuo futuro vuoi sapere?
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_2"); 
             }
-            if(dialoghiUIManager.FineDialogo("dialogue_veggente_2") == true && fine3 == false){
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_2") == true && fine3 == false){
                 fine3 = true;
-                dialoghiUIManager.StartDialoghi("dialogo_sueveggente_2"); //sto ascoltando
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_2"); 
             }
-            if(dialoghiUIManager.FineDialogo("dialogo_sueveggente_2") == true && fine4 == false){
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunke_2") == true && fine4 == false){
                 fine4 = true;
-                dialoghiUIManager.StartDialoghi("dialogue_veggente_3"); //dice qualcosa sul tuo futuro
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_3"); 
             }
-            if(dialoghiUIManager.FineDialogo("dialogue_veggente_3") == true && fine5 == false){
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_3") == true && fine5 == false){
                 fine5 = true;
-                dialoghiUIManager.StartDialoghi("dialogo_sueveggente_3"); //CHE VUOL DIREE
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_4");
+            }
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunke_4") == true && fine6 == false){
+                fine6 = true;
+                dialoghiUIManager.StartDialoghi("dialogue_drunk_5"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogue_drunk_5") == true && fine7 == false){
+                fine7 = true;
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_5"); 
+            }
+            if(dialoghiUIManager.FineDialogo("dialogo_suedrunk_5") == true && fine8 == false){
+                fine8 = true;
+                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_3"); 
                 player.GetComponent<PlayerMovement>().enabled = true;
                 rb.constraints = RigidbodyConstraints2D.None;
                 rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-                rb_veggente.constraints = RigidbodyConstraints2D.None;
-                rb_veggente.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rb_ubriacone.constraints = RigidbodyConstraints2D.None;
+                rb_ubriacone.constraints = RigidbodyConstraints2D.FreezeRotation;
                 anim2.SetBool("interact", false);
-                veggente.GetComponent<BoundedNPC>().enabled = true;
+                ubriacone.GetComponent<BoundedNPC>().enabled = true;
             }
 
-        }
+        }*/
+
 
     }
    
