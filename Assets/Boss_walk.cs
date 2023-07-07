c47eddfc71a5dde53bf46238bd97ca20dfce6e24
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-07-07 23:34:57 +0200

    Boss: si muove guardando il player

---
+++ Assets/Boss_walk.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_walk : StateMachineBehaviour
+{
+    public float speed = 2.5f;
+    Transform player;
+    Rigidbody2D rb;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 target = new Vector3(player.position.x, player.position.y);
+        animator.SetFloat("moveX", player.position.x - animator.transform.position.x);
+        animator.SetFloat("moveY", player.position.y - animator.transform.position.y);
+        Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+}
