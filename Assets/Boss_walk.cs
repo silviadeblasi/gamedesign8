6208e7a1b7ee4581d581d61112df001ebc2bd97e
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-07-07 23:46:04 +0200

    Boss: si avvicina ed entra nello stato di attacco

--- Assets/Boss_walk.cs
+++ Assets/Boss_walk.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Boss_walk : StateMachineBehaviour
 {
-    public float speed = 2.5f;
+    public float speed;
+    public float attackRange;
     Transform player;
     Rigidbody2D rb;
 
@@ -23,11 +24,16 @@ public class Boss_walk : StateMachineBehaviour
         animator.SetFloat("moveY", player.position.y - animator.transform.position.y);
         Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
+
+        if(Vector3.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("attack");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+       animator.ResetTrigger("attack");
     }
 }
