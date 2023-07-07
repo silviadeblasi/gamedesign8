d9cc8887b1afc2996973c9f0c8ece86e1676232f
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-07-07 03:21:55 +0200

    prove vita nemico

--- Assets/marghe_scene/Scripts/EnemyScripts/EnemyHealth.cs
+++ Assets/marghe_scene/Scripts/EnemyScripts/EnemyHealth.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public FloatValue maxHealth;
+    public float health;
+    private Animator myAnim;
+
+    private void Awake() 
     {
-        
+        health = (int)maxHealth.initialValue;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TakeDamage(float damage)
     {
-        
+        health -= damage;
+
+        if(health <= 0)
+        {
+            //this.gameObject.SetActive(false);
+            // Da implementare: animazione di morte
+            Debug.Log("Enemy died.");
+            myAnim.SetBool("dead", true);
+            GetComponent<Collider2D>().enabled = false;
+            this.enabled = false;
+        }
+    }
+
+    public void Knock(Rigidbody2D myRigidbody, float knockTime, float damage)
+    {
+        StartCoroutine(KnockCo(myRigidbody, knockTime));
+        TakeDamage(damage);
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D myRigidbody, float knockTime)
+    {
+        if(myRigidbody != null) //Controlla se l'enemy è ancora vivo
+        {
+            yield return new WaitForSeconds(knockTime);
+            myRigidbody.velocity = Vector2.zero;
+            myRigidbody.velocity = Vector2.zero;
+        }
+        else
+        {
+            myAnim.SetBool("dead", true);
+            GetComponent<Collider2D>().enabled = false;
+            this.enabled = false;
+        }
     }
 }
