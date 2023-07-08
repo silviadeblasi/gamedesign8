ef0617535713387cae0fe7c076b8b82da03f2e4b
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-07-08 19:19:36 +0200

    rifaccio inventario

--- Assets/marghe_scene/Scripts/Inventory/InventoryItem.cs
+++ Assets/marghe_scene/Scripts/Inventory/InventoryItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
 public class InventoryItem : ScriptableObject
 {
     public string itemName;
