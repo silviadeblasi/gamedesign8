c0fc3369af0dc76eeb07bf7e95547a6096b59e3a
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-07-10 22:44:45 +0200

    Revert "silvia mi ha detto di pushare"
    This reverts commit 6be4380d6e1cd0a94ef465f5b64f3cbb8edf35be.

--- Assets/marghe_scene/Scripts/Inventory/PhysicalInventoryItem.cs
+++ Assets/marghe_scene/Scripts/Inventory/PhysicalInventoryItem.cs
@@ -6,20 +6,14 @@ public class PhysicalInventoryItem : MonoBehaviour
 {
     [SerializeField] private PlayerInventory playerInventory;
     [SerializeField] private InventoryItem thisItem;
-    [SerializeField] private GameObject canvaInventory;
-
-    private void Awake()
-    {
-        canvaInventory.gameObject.SetActive(false);
-    }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {    
         if(other.gameObject.CompareTag("Player") && !other.isTrigger)
         {
             AddItemToInventory();
-            this.gameObject.SetActive(false);
-            //Destroy(this.gameObject);
+            //this.gameObject.SetActive(false);
+            Destroy(this.gameObject);
         }
     }
 
@@ -38,11 +32,4 @@ public class PhysicalInventoryItem : MonoBehaviour
             }
         }
     }
-
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(5);
-        canvaInventory.gameObject.SetActive(false);
-    }
-
 }
