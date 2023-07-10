b4dab4a656ecf3e6c9ccc06e58d311068b85dfcc
silviadeblasi <62344404+silviadeblasi@users.noreply.github.com>
2023-07-10 22:06:28 +0200

    viallger_1 parla

--- Assets/silvia_scene/script_sylvia/NPC_interactable.cs
+++ Assets/silvia_scene/script_sylvia/NPC_interactable.cs
@@ -7,15 +7,17 @@ public class NPC_interactable : MonoBehaviour
     private Animator anim;
     private Animator anim_veggente;
     private Animator anim_ubriacone;
-    private Animator anim_village_pistola;
+    private Animator anim_village_1;
     public DialoghiUIManager dialoghiUIManager; //cerco i dialoghi e li faccio comparire
     public GameObject player;
     public Rigidbody2D rb_player;
     public GameObject veggente;
     public GameObject ubriacone;
-    public GameObject villager_pistola;
+    public GameObject villager_1;
     private Rigidbody2D rb_veggente;
     private Rigidbody2D rb_ubriacone;
+    private Rigidbody2D rb_villager_1;
+
     
     private bool fine1 = false;
     private bool fine2 = false;
@@ -35,9 +37,12 @@ public class NPC_interactable : MonoBehaviour
         anim = GetComponent<Animator>();
         anim_veggente = veggente.GetComponent<Animator>();
         anim_ubriacone = ubriacone.GetComponent<Animator>();
+        anim_village_1 = villager_1.GetComponent<Animator>();
         rb_player = GetComponent<Rigidbody2D>();
         rb_veggente = veggente.GetComponent<Rigidbody2D>();
         rb_ubriacone = ubriacone.GetComponent<Rigidbody2D>();
+        rb_villager_1 = villager_1.GetComponent<Rigidbody2D>();
+
     }
 
     private void OnTriggerStay2D(Collider2D other) {
@@ -59,6 +64,10 @@ public class NPC_interactable : MonoBehaviour
                 fine6 = false;
                 fine7 = false;
                 fine8 = false;
+                fine9 = false;
+                fine10 = false;
+                fine11 = false;
+
                 rb_player.constraints = RigidbodyConstraints2D.FreezeAll;
                 player.GetComponent<PlayerMovement>().enabled = false;
                 dialoghiUIManager.StartDialoghi("dg_sh_1"); //i bet you are looking for...
@@ -121,7 +130,7 @@ public class NPC_interactable : MonoBehaviour
                 fine9 = false;
                 fine10 = false;
                 fine11 = false;
-                
+
                 rb_player.constraints = RigidbodyConstraints2D.FreezeAll;
                 player.GetComponent<PlayerMovement>().enabled = false;
                 dialoghiUIManager.StartDialoghi("dg_dk_1"); 
@@ -175,7 +184,13 @@ public class NPC_interactable : MonoBehaviour
 
         }
 
-        /*if(other.gameObject.layer == 21){ //villager pistola
+        if(other.gameObject.layer == 22){ //villager_1
+                Vector3 dir = villager_1.GetComponent<BoundedNPC>().directionVector;
+                anim_village_1.SetBool("interact", true);
+                anim_village_1.SetFloat("moveX", dir.x);
+                anim_village_1.SetFloat("moveY", dir.y);
+                rb_villager_1.constraints = RigidbodyConstraints2D.FreezeAll;
+                villager_1.GetComponent<BoundedNPC>().enabled = false;
         
             if(Input.GetKeyDown(KeyCode.X)){
                 fine1 = false;
@@ -183,57 +198,26 @@ public class NPC_interactable : MonoBehaviour
                 fine3 = false;
                 fine4 = false;
                 fine5 = false;
-                Vector3 dir = ubriacone.GetComponent<BoundedNPC>().directionVector;
-                anim2.SetBool("interact", true);
-                anim2.SetFloat("moveX", dir.x);
-                anim2.SetFloat("moveY", dir.y);
-                rb_ubriacone.constraints = RigidbodyConstraints2D.FreezeAll;
-                ubriacone.GetComponent<BoundedNPC>().enabled = false;
-                rb.constraints = RigidbodyConstraints2D.FreezeAll;
+                fine6 = false;
+                fine7 = false;
+                fine8 = false;
+                fine9 = false;
+                fine10 = false;
+                fine11 = false;
+
+                
+                rb_player.constraints = RigidbodyConstraints2D.FreezeAll;
                 player.GetComponent<PlayerMovement>().enabled = false;
-                dialoghiUIManager.StartDialoghi("dialogue_drunk_1"); 
+                dialoghiUIManager.StartDialoghi("dg_ct_1"); 
             }
-            if(dialoghiUIManager.FineDialogo("dialogue_drunk_1") == true && fine1 == false){
-                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_1"); 
+            if(dialoghiUIManager.FineDialogo("dg_ct_1") == true && fine1 == false){ 
                 fine1 = true;
-            }
-            if(dialoghiUIManager.FineDialogo("dialogo_suedrunk_1") == true && fine2 == false){
-                fine2 = true;
-                dialoghiUIManager.StartDialoghi("dialogue_drunk_2"); 
-            }
-            if(dialoghiUIManager.FineDialogo("dialogue_drunk_2") == true && fine3 == false){
-                fine3 = true;
-                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_2"); 
-            }
-            if(dialoghiUIManager.FineDialogo("dialogo_suedrunke_2") == true && fine4 == false){
-                fine4 = true;
-                dialoghiUIManager.StartDialoghi("dialogue_drunk_3"); 
-            }
-            if(dialoghiUIManager.FineDialogo("dialogue_drunk_3") == true && fine5 == false){
-                fine5 = true;
-                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_4");
-            }
-            if(dialoghiUIManager.FineDialogo("dialogo_suedrunke_4") == true && fine6 == false){
-                fine6 = true;
-                dialoghiUIManager.StartDialoghi("dialogue_drunk_5"); 
-            }
-            if(dialoghiUIManager.FineDialogo("dialogue_drunk_5") == true && fine7 == false){
-                fine7 = true;
-                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_5"); 
-            }
-            if(dialoghiUIManager.FineDialogo("dialogo_suedrunk_5") == true && fine8 == false){
-                fine8 = true;
-                dialoghiUIManager.StartDialoghi("dialogo_suedrunk_3"); 
                 player.GetComponent<PlayerMovement>().enabled = true;
-                rb.constraints = RigidbodyConstraints2D.None;
-                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-                rb_ubriacone.constraints = RigidbodyConstraints2D.None;
-                rb_ubriacone.constraints = RigidbodyConstraints2D.FreezeRotation;
-                anim2.SetBool("interact", false);
-                ubriacone.GetComponent<BoundedNPC>().enabled = true;
+                rb_player.constraints = RigidbodyConstraints2D.None;
+                rb_player.constraints = RigidbodyConstraints2D.FreezeRotation;
             }
 
-        }*/
+        }
 
 
     }
@@ -251,6 +235,13 @@ public class NPC_interactable : MonoBehaviour
             rb_ubriacone.constraints = RigidbodyConstraints2D.None;
             rb_ubriacone.constraints = RigidbodyConstraints2D.FreezeRotation;
             ubriacone.GetComponent<BoundedNPC>().enabled = true;
+        }
+
+        if(other.gameObject.layer == 22){ // villager_1
+            anim_village_1.SetBool("interact", false);
+            rb_villager_1.constraints = RigidbodyConstraints2D.None;
+            rb_villager_1.constraints = RigidbodyConstraints2D.FreezeRotation;
+            villager_1.GetComponent<BoundedNPC>().enabled = true;
         }
     }
 
