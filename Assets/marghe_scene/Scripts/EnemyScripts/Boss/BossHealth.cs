831891c001b9a7066e77e83e1ad0497ac8b58d98
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-07-11 03:17:14 +0200

    Boss: infligge danno

--- Assets/marghe_scene/Scripts/EnemyScripts/Boss/BossHealth.cs
+++ Assets/marghe_scene/Scripts/EnemyScripts/Boss/BossHealth.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class BossHealth : MonoBehaviour
 {
-    [SerializeField] private int health = 20;
+    public float currentHealth;
+    public int maxHealth;
     [SerializeField] private Animator animator;
     
     public void TakeDamage(int damage)
     {
-        health -= damage;
-        if (health <= 0)
+        currentHealth -= damage;
+        if (currentHealth <= 0)
         {
             animator.SetBool("died", true);
             gameObject.SetActive(false);
