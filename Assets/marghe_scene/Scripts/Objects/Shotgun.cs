f516c19253789b93633c39286b3ebac07f34808d
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-08-13 15:03:08 +0200

    Pistola: limite distanza proiettile

--- Assets/marghe_scene/Scripts/Objects/Shotgun.cs
+++ Assets/marghe_scene/Scripts/Objects/Shotgun.cs
@@ -6,10 +6,21 @@ public class Shotgun : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D myRigidbody;
+    public float lifetime;
+    private float lifetimeCounter;
     
     void Start()
     {
-        
+        lifetimeCounter = lifetime;
+    }
+
+    public void Update()
+    {
+        lifetimeCounter -= Time.deltaTime;
+        if (lifetimeCounter <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void Setup(Vector2 velocity, Vector3 direction)
