d00c71749e8ff1a5cbc646ebd86b7b769779e47e
MargheritaMorel <129385123+MargheritaMorel@users.noreply.github.com>
2023-09-06 12:50:55 +0200

    Stagger 1

--- Assets/marghe_scene/Scripts/EnemyScripts/EnemyHealth.cs
+++ Assets/marghe_scene/Scripts/EnemyScripts/EnemyHealth.cs
@@ -7,14 +7,23 @@ public class EnemyHealth : MonoBehaviour
     public float currentHelath;
     public int maxHealth;
     public EnemyController1 enemyController1;
+    public Animator myAnim;
 
     public void Damage(float damage)
     {
         currentHelath -= damage;
+        StartCoroutine(DamageCo());
         if(currentHelath <= 0)
         {
             enemyController1.Die();
             gameObject.SetActive(false); //non posso distruggerlo
         }
+    }
+
+    private IEnumerator DamageCo()
+    {
+        myAnim.SetBool("stagger", true);
+        yield return null;
+        myAnim.SetBool("stagger", false);
     }
 }
